28e2540f6b66b77a3253fcc8c46cd7331e0909e3
AdamAbdulkareeem <dmabdulkareem@gmail.com>
2026-07-29 20:10:11 +0100

    feat: Implement unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -22,23 +22,64 @@ const Ranking = ({ description, rating }) => {
       {description} {rating}
     </div>
   )
-
 }
 
+const Total = ({ total }) => {
+  return (
+    <p>all {total}</p>
+  )
+}
+
+const Average = ({ positiveRating, negativeRating, total }) => {
+  if (total == 0) {
+    return (
+      "No feedback given"
+    )
+  }
+  const average = (positiveRating - negativeRating) / total
+  return (
+    <div>
+      average {average}
+    </div>
+  )
+}
+const Percent = ({ positiveRating, total }) => {
+  if (total == 0) {
+    return (
+      <div>
+        Positive: "No feedback given"
+      </div>
+
+    )
+  }
+  const percentage = (positiveRating * 100) / total
+  return (
+    <div>
+      positive {percentage} %
+    </div>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const setToGood = () => {
-    setGood(good + 1)
+    const updateGood = good + 1
+    setGood(updateGood)
+    setAll(all + 1)
   }
   const setToNeutral = () => {
-    setNeutral(neutral + 1)
+    const updateNeutral = neutral + 1
+    setNeutral(updateNeutral)
+    setAll(all + 1)
   }
   const setToBad = () => {
-    setBad(bad + 1)
+    const updateBad = bad + 1
+    setBad(updateBad)
+    setAll(all + 1)
   }
 
   return (
@@ -51,9 +92,15 @@ const App = () => {
       </div>
 
       <Header title="Statistics" />
-      <Ranking description="good" rating={good} />
-      <Ranking description="neutral" rating={neutral} />
-      <Ranking description="bad" rating={bad} />
+      <div className="ranking">
+        <Ranking description="good" rating={good} />
+        <Ranking description="neutral" rating={neutral} />
+        <Ranking description="bad" rating={bad} />
+        <Total total={all} />
+        <Average positiveRating={good} negativeRating={bad} total={all} />
+        <Percent positiveRating={good} total={all} />
+      </div>
+
     </div>
   )
 }
