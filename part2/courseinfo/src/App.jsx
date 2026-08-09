2dc2f9512d671f05f2ab31a2da7d11adada5cbe7
AdamAbdulkareeem <dmabdulkareem@gmail.com>
2026-08-09 13:20:15 +0100

    feat: 2.1: Course information step 6

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,18 +1,24 @@
 const Header = (props) => <h1>{props.course}</h1>
 
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
 
-const Content = (props) => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-)
+const Parts = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <p key={part.id}>{part.name} {part.exercises}</p>
+      )}
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Parts parts={parts} />
+    </div>
+  )
+}
+
 
 const Course = ({ course }) => {
   return (
